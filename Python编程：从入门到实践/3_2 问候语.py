d60e393329a1eaf25d5b8bcc449f65ad61eb92d4
wjingyuan <wjywangyiyouxiang@163.com>
2018-07-02 22:03:17 +0800

    《Python编程：从入门到实践》第三章练习

---
+++ Python编程：从入门到实践/3_2 问候语.py
@@ -0,0 +1,6 @@
+# 有列表：names = ['Alex', 'Black Girl', 'Peiqi']。
+# 为每人打印一条消息。 每条消息都包含相同的问候语， 但抬头为相应朋友的姓名。
+
+names = ['Alex', 'Black Girl', 'Peiqi']
+for i in names:
+    print('{},how are you?'.format(i))
